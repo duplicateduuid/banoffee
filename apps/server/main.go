efb59dd5fc4da1967582df6388ec211ca693b902
Guilherme775 <guilhermereis28122002@gmail.com>
2024-03-13 18:34:55 -0300

    feat: initial server setup

--- apps/server/main.go
+++ apps/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Banoffee")
+	// redis.NewClient(&redis.Options{
+	// 	Addr:     "localhost:6379",
+	// 	Password: "", // no password set
+	// 	DB:       0,  // use default DB
+	// })
+
+	// _, err := sqlx.Connect("postgres", "user=postgres dbname=go-api password=password sslmode=disable")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	router := gin.Default()
+
+	router.GET("/health-check", func(ctx *gin.Context) { ctx.JSON(200, gin.H{"message": "Banoffee"}) })
+
+	router.Run("localhost:8080")
 }
