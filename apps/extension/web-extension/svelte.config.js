b94e5d3c0211a1624be99fbdda8d715b2011766e
KindaSloth <guilhermereis28122002@gmail.com>
2024-03-19 01:45:53 -0300

    feat: init web extension

---
+++ apps/extension/web-extension/svelte.config.js
@@ -0,0 +1,11 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		appDir: 'app'
+	}
+};
+
+export default config;
