82416d1b7d6d30e3c96897b3468b34bccec20a9a
henri <henri@henr.in>
2024-03-18 23:57:36 -0300

    feat: make usernames unique

--- migrations/20240313220125_create_user_table.sql
+++ migrations/20240313220125_create_user_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE "user" (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    username VARCHAR(255),
+    username VARCHAR(255) UNIQUE NOT NULL,
     avatar_url VARCHAR(255),
     header_url VARCHAR(255),
     bio VARCHAR(255),
