235da721bf2cc953b95dddd0f61c5385cfcf8ef8
KindaSloth <guilhermereis28122002@gmail.com>
2024-03-20 22:44:37 -0300

    feat: add web extension loading

--- apps/extension/web-extension/src/routes/+layout.svelte
+++ apps/extension/web-extension/src/routes/+layout.svelte
@@ -2,6 +2,6 @@
 	import '../app.css';
 </script>
 
-<main class="min-w-[20em] min-h-[30em] font-secondary bg-[#F7F6F1] text-[#18171C]">
+<main class="font-secondary bg-[#F7F6F1] text-[#18171C]">
 	<slot />
 </main>
--- apps/extension/web-extension/src/routes/+page.svelte
+++ apps/extension/web-extension/src/routes/+page.svelte
@@ -1,31 +1,210 @@
 <script lang="ts">
-  let isLoggedIn: boolean = $state(false);
+  let loading: boolean = $state(true);
 
   // TODO: test it on firefox
   $effect(() => {
     if (typeof chrome !== "undefined") {
       chrome.storage.local.get("sessionId", (data) => {
         if (data.sessionId) {
-          isLoggedIn = true;
+          loading = false;
+        } else {
+          // Handle not logged in user
         }
       });
     } else if (typeof browser !== "undefined") {
       browser.storage.local.get("sessionId", (data) => {
         if (data.sessionId) {
-          isLoggedIn = true;
+          loading = false;
+        } else {
+          // Handle not logged in user
         }
       });
     }
   });
 </script>
 
-<div>
-  {#if isLoggedIn}
-    <p class="text-red-500">isLoggedIn</p>
-  {:else}
-    <p class="text-red-500">isNotLoggedIn</p>
+<div class="w-full h-full">
+  {#if loading}
+    <div class="min-w-[30em] min-h-[20em]">
+      <div id="load">
+        <div>G</div>
+        <div>N</div>
+        <div>I</div>
+        <div>D</div>
+        <div>A</div>
+        <div>O</div>
+        <div>L</div>
+      </div>
+    </div>
+  {/if}
+
+  {#if !loading}
+    <div class="min-w-[30em] min-h-[20em] flex flex-col justify-between p-8">
+      <h1>Logo</h1>
+
+      <h1>Title</h1>
+
+      <h1>Bookmark</h1>
+    </div>
   {/if}
 </div>
 
+<!-- TODO: maybe rewrite it using tailwind (I'm lazy, sorry) -->
 <style>
+  #load {
+    position: absolute;
+    width: 600px;
+    height: 36px;
+    left: 50%;
+    top: 45%;
+    margin-left: -300px;
+    overflow: visible;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    cursor: default;
+  }
+
+  #load div {
+    position: absolute;
+    width: 20px;
+    height: 36px;
+    opacity: 0;
+    font-family: Helvetica, Arial, sans-serif;
+    animation: move 2.5s linear infinite;
+    -o-animation: move 2.5s linear infinite;
+    -moz-animation: move 2.5s linear infinite;
+    -webkit-animation: move 2.5s linear infinite;
+    transform: rotate(180deg);
+    -o-transform: rotate(180deg);
+    -moz-transform: rotate(180deg);
+    -webkit-transform: rotate(180deg);
+    color: #4e473b;
+  }
+
+  #load div:nth-child(2) {
+    animation-delay: 0.2s;
+    -o-animation-delay: 0.2s;
+    -moz-animation-delay: 0.2s;
+    -webkit-animation-delay: 0.2s;
+  }
+  #load div:nth-child(3) {
+    animation-delay: 0.4s;
+    -o-animation-delay: 0.4s;
+    -webkit-animation-delay: 0.4s;
+    -webkit-animation-delay: 0.4s;
+  }
+  #load div:nth-child(4) {
+    animation-delay: 0.6s;
+    -o-animation-delay: 0.6s;
+    -moz-animation-delay: 0.6s;
+    -webkit-animation-delay: 0.6s;
+  }
+  #load div:nth-child(5) {
+    animation-delay: 0.8s;
+    -o-animation-delay: 0.8s;
+    -moz-animation-delay: 0.8s;
+    -webkit-animation-delay: 0.8s;
+  }
+  #load div:nth-child(6) {
+    animation-delay: 1s;
+    -o-animation-delay: 1s;
+    -moz-animation-delay: 1s;
+    -webkit-animation-delay: 1s;
+  }
+  #load div:nth-child(7) {
+    animation-delay: 1.2s;
+    -o-animation-delay: 1.2s;
+    -moz-animation-delay: 1.2s;
+    -webkit-animation-delay: 1.2s;
+  }
+
+  @keyframes move {
+    0% {
+      left: 0;
+      opacity: 0;
+    }
+    35% {
+      left: 41%;
+      -moz-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      transform: rotate(0deg);
+      opacity: 1;
+    }
+    65% {
+      left: 59%;
+      -moz-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      transform: rotate(0deg);
+      opacity: 1;
+    }
+    100% {
+      left: 100%;
+      -moz-transform: rotate(-180deg);
+      -webkit-transform: rotate(-180deg);
+      -o-transform: rotate(-180deg);
+      transform: rotate(-180deg);
+      opacity: 0;
+    }
+  }
+
+  @-moz-keyframes move {
+    0% {
+      left: 0;
+      opacity: 0;
+    }
+    35% {
+      left: 41%;
+      -moz-transform: rotate(0deg);
+      transform: rotate(0deg);
+      opacity: 1;
+    }
+    65% {
+      left: 59%;
+      -moz-transform: rotate(0deg);
+      transform: rotate(0deg);
+      opacity: 1;
+    }
+    100% {
+      left: 100%;
+      -moz-transform: rotate(-180deg);
+      transform: rotate(-180deg);
+      opacity: 0;
+    }
+  }
+
+  @-webkit-keyframes move {
+    0% {
+      left: 0;
+      opacity: 0;
+    }
+    35% {
+      left: 41%;
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+      opacity: 1;
+    }
+    65% {
+      left: 59%;
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+      opacity: 1;
+    }
+    100% {
+      left: 100%;
+      -webkit-transform: rotate(-180deg);
+      transform: rotate(-180deg);
+      opacity: 0;
+    }
+  }
+
+  @-o-keyframes move {
+    0% {
+      left: 0;
+      opacity: 0;
+    }
+  }
 </style>
