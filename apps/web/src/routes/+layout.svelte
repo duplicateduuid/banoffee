147f3575d0ca16cd8e68a24cbca416ef83eaeae1
henri <henri@henr.in>
2024-03-30 23:07:49 -0300

    chore: update yarn.lock

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,12 +1,13 @@
-<script>
-	import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+<script lang="ts">
+	import { QueryClientProvider } from "@tanstack/svelte-query";
 	import '../app.css';
+	import type { LayoutData } from "./$types";
 
-	const queryClient = new QueryClient();
+	export let data: LayoutData;
 </script>
 
 <main class="w-full min-h-screen font-secondary bg-[#F7F6F1] text-[#18171C]">
-	<QueryClientProvider client={queryClient}>
+	<QueryClientProvider client={data.queryClient}>
 		<slot />
 	</QueryClientProvider>
 </main>
--- apps/web/src/routes/+page.ts
+++ apps/web/src/routes/+page.ts
@@ -2,10 +2,18 @@ import { superValidate } from "sveltekit-superforms";
 import type { PageLoad } from "./$types";
 import { zod } from "sveltekit-superforms/adapters";
 import { signInRequestSchema } from "../requests/auth";
+import { me } from "../requests/user";
 
-export const load: PageLoad = (async () => {
+export const load: PageLoad = async ({ parent }) => {
+  const { queryClient } = await parent();
+
   const signInForm = await superValidate(zod(signInRequestSchema));
   const signUpForm = await superValidate(zod(signInRequestSchema));
 
-  return { signInForm, signUpForm };
-});
+  const user = await queryClient.prefetchQuery({
+    queryKey: ['me'],
+    queryFn: me
+  })
+
+  return { signInForm, signUpForm, user };
+};
