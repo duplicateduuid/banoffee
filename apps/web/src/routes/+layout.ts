3b1642a290f7ca0de5553adb7e6b9256c4f2469f
Guilherme775 <guilhermereis28122002@gmail.com>
2024-04-03 17:12:37 -0300

    fix: Henri shit

--- apps/web/src/routes/+layout.ts
+++ apps/web/src/routes/+layout.ts
@@ -1,18 +1,23 @@
-import { QueryClient } from "@tanstack/svelte-query";
-import { browser } from "$app/environment";
-import type { LayoutLoad, LayoutLoadEvent } from "./$types";
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { QueryClient } from '@tanstack/svelte-query';
+import { browser } from '$app/environment';
+import type { LayoutLoad, LayoutLoadEvent } from './$types';
+import { signInRequestSchema, signUpRequestSchema } from '../requests/auth';
 
 export const load: LayoutLoad = async ({ data }: LayoutLoadEvent) => {
-  const { user } = data;
-  
-  const queryClient = new QueryClient({
-  	defaultOptions: {
-  		queries: { 
-        enabled: browser,
-        staleTime: Infinity
-      },
-  	}
-  });
+	const { user } = data;
+	const signInForm = await superValidate(zod(signInRequestSchema));
+	const signUpForm = await superValidate(zod(signUpRequestSchema));
 
-  return { queryClient, user }
-}
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser,
+				staleTime: Infinity
+			}
+		}
+	});
+
+	return { queryClient, user, signInForm, signUpForm };
+};
--- apps/web/src/routes/+page.ts
+++ apps/web/src/routes/+page.ts
@@ -1,12 +1,7 @@
-import { superValidate } from "sveltekit-superforms";
 import type { PageLoad } from "./$types";
-import { zod } from "sveltekit-superforms/adapters";
-import { signInRequestSchema, signUpRequestSchema } from "../requests/auth";
 
 export const load: PageLoad = async ({ parent }) => {
-  const { user }= await parent();
-  const signInForm = await superValidate(zod(signInRequestSchema));
-  const signUpForm = await superValidate(zod(signUpRequestSchema));
+  const { user } = await parent();
   
-  return { user, signInForm, signUpForm };
+  return { user };
 };
