174dec0bf2734ff0a01beb6be7135a7ca47c7395
Guilherme775 <guilhermereis28122002@gmail.com>
2024-04-03 18:10:58 -0300

    feat: add infinite loading on search page

--- apps/web/src/routes/search/+page.svelte
+++ apps/web/src/routes/search/+page.svelte
@@ -6,6 +6,8 @@
 	import { createQuery, createInfiniteQuery } from '@tanstack/svelte-query';
 	import Card from '$lib/components/Card.svelte';
 	import SkeletonCard from '$lib/components/SkeletonCard.svelte';
+	import classNames from 'classnames';
+	import Spinner from '$lib/components/Spinner.svelte';
 
 	const search = $page.url.searchParams.get('search');
 	const isURL = isValidUrl(search || '');
@@ -29,7 +31,7 @@
 		getNextPageParam: (lastPage, allPages) => {
 			if (!lastPage) return undefined;
 
-			return lastPage.length !== 0 ? allPages.length * 10 : undefined;
+			return lastPage.length === 10 ? allPages.length * 10 : undefined;
 		},
 		enabled: !isURL,
 		staleTime: 0,
@@ -42,6 +44,29 @@
 			}
 
 			return data;
+		}
+	});
+
+	let observerElem: HTMLDivElement | null = $state(null);
+
+	$effect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				const [target] = entries;
+
+				if (target.isIntersecting && $searchQuery.hasNextPage) {
+					$searchQuery.fetchNextPage();
+				}
+			},
+			{ threshold: 0 }
+		);
+
+		if (observerElem) {
+			observer.observe(observerElem);
+
+			return () => {
+				observer.unobserve(observerElem!);
+			};
 		}
 	});
 </script>
@@ -77,7 +102,17 @@
 				{/each}
 			{/each}
 
-			<button on:click={() => $searchQuery.fetchNextPage()}>Get more</button>
+			<div
+				class={classNames(
+					'grid h-12 w-full place-content-center',
+					!$searchQuery.hasNextPage && 'hidden'
+				)}
+				bind:this={observerElem}
+			>
+				{#if $searchQuery.isFetchingNextPage && $searchQuery.hasNextPage}
+					<Spinner />
+				{/if}
+			</div>
 		</div>
 	{/if}
 </section>
