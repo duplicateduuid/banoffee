4153592ebdf5a773039b97c2f5c5b15110eef208
Guilherme775 <guilhermereis28122002@gmail.com>
2024-04-02 23:15:51 -0300

    feat: add card component

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -2,10 +2,12 @@
 	import { createQuery } from '@tanstack/svelte-query';
 	import { getRecommendations } from '../requests/user';
 	import { type Resource } from '../schemas/resource';
+	import Card from './Card.svelte';
 
 	const recommendations = createQuery<Resource[]>({
 		queryKey: ['recommendations'],
-		queryFn: getRecommendations
+		queryFn: getRecommendations,
+		retry: false
 	});
 </script>
 
@@ -20,23 +22,7 @@
 		<!-- TODO: add loading state/UI -->
 		{#if !$recommendations.isPending && $recommendations.data}
 			{#each $recommendations.data as recommendation, i}
-				<!-- TODO: move to a card component -->
-				<a href={'/resource-' + i} class="flex gap-4 h-40 shadow px-8 py-6 rounded-lg bg-white">
-					<span class="bg-primary-400 h-full aspect-video rounded-lg" />
-					<div class="flex flex-col justify-between h-full">
-						<div class="flex flex-col">
-							<p class="font-primary text-2xl tracking-wider font-semibold">
-								{recommendation.name}
-							</p>
-							{#if recommendation.description}
-								<p class="text-sm line-clamp-2">{recommendation.description}</p>
-							{/if}
-						</div>
-						{#if recommendation.author}
-							<p>{recommendation.author}</p>
-						{/if}
-					</div>
-				</a>
+				<Card redirect="/resource" name={recommendation.name} description={recommendation.description} author={recommendation.author} />
 			{/each}
 		{/if}
 	</div>
